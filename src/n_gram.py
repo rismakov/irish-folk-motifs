b77c497fdcd79d95fbb5bc60e4f405a3a55d2b75
rismakov <rebekkah@Rebekkahs-MacBook-Pro.local>
2017-01-29 18:40:05 -0800

    Rebekkah-edited

--- src/n_gram.py
+++ src/n_gram.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 from math import log
 
-
-
 class NGramModel(object):
     """
     Takes a series from a DataFrame and an n and trains a model of a given n.
@@ -31,6 +29,11 @@ class NGramModel(object):
         # defaultdict of defaultdict that holds the frequencies of a given suffix.
         self.frequencies = None
 
+    def get_window_properties(tune,i):
+        window = tune[i:i + self.n]
+        given = window[:self.n-1]
+        following = window[-1]
+        return given, following
 
     def fit(self, series):
         # For one character at a time.
@@ -45,21 +48,20 @@ class NGramModel(object):
             # Create dictionary of Counter() with the n-gram as key and the
             # follwing next letter as key and the count of that occuring as
             # value.
-            self.n_grams_count = defaultdict(lambda: Counter())
+            self.n_grams_count = defaultdict(lambda: Counter()) #R.I.: why is the lambda necessary? I think it can be written without it
             # Go through each tune.
             for tune in series.values:
                 tune = self._pad_string(tune)
 
+                #for i, sound in enumerate(tune[:-self.n]): (maybe personal choice, but I think enumerate tends to look a bit cleaner than xrange)
                 for i in xrange(len(tune) - self.n + 1):
-                    window = tune[i:i + self.n]
-                    given = window[:self.n-1]
-                    following = window[-1]
+                    given, following = self.get_window_properties(tune,i)
                     self.n_grams_count[given][following] += 1
 
             self._create_frequency()
 
 
-    def _pad_string(self, string):
+    def _pad_string(self, string): # R.I.: add docstring, not sure why string needs to be padded
         if self.n == 1:
             return string
         else:
@@ -68,15 +70,18 @@ class NGramModel(object):
             string = pad + string + pad
             return string
 
+    def get_frequency_dict(counter):
+        total_n = float(sum(counter.values()))
+        return {k: v/total_n for k, v in counter.iteritems()}
+
     # Calculates the frequency of a given n-gram.
     def _create_frequency(self):
         n_total = 0
         #for n size 1 its simpler and faster.
         if self.n == 1:
-            self.frequencies = defaultdict(lambda: 0.0)
-            total = float(sum(self.n_grams_count.values()))
+            self.frequencies = defaultdict(lambda: 0.0) # is this not the same as defaultdict(float) ?
             #predict method.
-            frequencies = {k: v/total for k, v in self.n_grams_count.iteritems()}
+            frequencies = self.get_frequency_dict(self.n_grams_count)
             self.frequencies.update(frequencies)
 
         if self.n >1:
@@ -84,11 +89,10 @@ class NGramModel(object):
             # occurance of a letter after an n-gram and returns the
             # frequency of that. Update self.frequencies
             total_n = 0
-            self.frequencies = defaultdict(lambda: defaultdict(lambda: 0.0))
+            self.frequencies = defaultdict(lambda: defaultdict(lambda: 0.0)) #again dont think the lambdas are necessary
             for given, counter in self.n_grams_count.iteritems():
                 frequencies = {}
-                total_n = float(sum(counter.values()))
-                frequencies[given] = {k: v/total_n for k, v in counter.iteritems()}
+                frequencies[given] = self.get_frequency_dict(counter)
                 self.frequencies.update(frequencies)
 
 #Doesn't work yet.
@@ -97,10 +101,8 @@ class NGramModel(object):
         #import pdb; pdb.set_trace()
         sum_of_log_probs = 0
         working_tune = self._pad_string(new_tune)
-        for i in xrange(len(working_tune) - self.n):
-            window = working_tune[i:i + self.n]
-            history = window[:self.n - 1]
-            token = window[-1]
+        for i in xrange(len(working_tune) - self.n): # for i, sound in enumerate(working_tune[:-self.n])
+            history, token = self.get_window_properties(working_tune,i)
             probability = self.frequencies[history][token]
             sum_of_log_probs += log(probability + 1e-10)
 
